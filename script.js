ac0ba54c3165673a97ab497a7582741582e313b8
Etec <etec@etec.com>
2024-08-14 11:42:33 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const btnCadastrar = document.querySelector('#cadastrar');
 const btnAuto_cadastrar = document.querySelector("#auto_casdastrar");
 const btnImprimir = document.querySelector('#imprimir');
-var nome = [];
-var idade = [];
-var curso =[];
+var nome;
+var idade;
+var curso;
 console.log(nome)
 btnCadastrar.addEventListener('click', ()=>{
     nome.push(document.querySelector('#nome').value);''
 })
+function click(){
+    
+}
